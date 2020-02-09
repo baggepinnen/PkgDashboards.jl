93a15ec5af9e88a3d4d7a38c558abd370f96a15b
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2020-02-09 16:36:48 +0800

    turn off autoopen in test

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,12 +6,17 @@ using Test
     db = dashboard("mfalt", output=:markdown, autoopen=false)
     @test db isa PkgDashboards.DashBoard
     db = dashboard("mfalt", output=:html, autoopen=false)
-    io = IOBuffer()
-    for m in [MIME"text/plain",MIME"text/markdown",MIME"text/html"]
-        @test_nowarn show(io, m(), db)
+    @testset "show" begin
+        @info "Testing show"
+        io = IOBuffer()
+        for m in [MIME"text/plain",MIME"text/markdown",MIME"text/html"]
+            @test_nowarn show(io, m(), db)
+        end
     end
 
-    @test_nowarn db = dashboard("mfalt", output=:markdown, autoopen=true, stargazers=true, activity=true, githubci=true)
-
-    @test_nowarn db = pkgdashboard("Hyperopt", output=:markdown, autoopen=false)
+    @test_nowarn db = dashboard("mfalt", output=:markdown, autoopen=false, stargazers=true, activity=true, githubci=true)
+    @testset "pkgdashboard" begin
+        @info "Testing pkgdashboard"
+        @test_nowarn db = pkgdashboard("Hyperopt", output=:markdown, autoopen=false)
+    end
 end
